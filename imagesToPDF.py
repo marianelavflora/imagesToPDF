bfb48a82237dacaecf0e4d0f6db1b7da371c949d
Marianela Flora <marianelavflora@gmail.com>
2023-02-21 14:26:21 -0300

    PNG support, A4 styled, clean coded
    Added PNG support for image merging.
    
    A4 styled for better compatibility
    
    Changed some variable names to be easily recognizable.

--- imagesToPDF.py
+++ imagesToPDF.py
@@ -1,15 +1,20 @@
 import img2pdf, os
 
-directoryName = "/testingEnvironment"
-imgs = []
+directoryName = "/select/path"
+images = []
+
+A4Size = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+A4Layout = img2pdf.get_layout_fun(A4Size)
 
 for fileName in os.listdir(directoryName):
-	if not fileName.endswith(".jpg"):
+	if not fileName.endswith(".jpg") or fileName.endswith(".png"):
 		continue
 	path = os.path.join(directoryName, fileName)
 	if os.path.isdir(path):
 		continue
-	imgs.append(path)
+	images.append(path)
 
-with open("name.pdf","wb") as f:
-	f.write(img2pdf.convert(imgs))
+with open("Result.pdf","wb") as f:
+	f.write(img2pdf.convert(images, layout=A4Layout))
+
+
